6bad6d33a4ec62595f6f7b5b021eaeff721483b3
onur Altunsoy <onuraltunsoy28@gmail.com>
2019-07-17 12:48:44 +0300

    inital commit

---
+++ src/main/java/com/altunsoy/EdicApplication.java
@@ -0,0 +1,13 @@
+package com.altunsoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdicApplication.class, args);
+	}
+
+}
